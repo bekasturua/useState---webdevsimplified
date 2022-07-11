6e7f0f8d5037d0b7e6d98341f0ab4319f3e4e19c
Beka Sturua <beqasturua3@gmail.com>
2022-07-11 20:54:01 +0400

    finished the review of the useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
 
-function countInitial() {
-  return 4;
-}
-
 function App() {
-  const [count, setCount] = useState(countInitial());
+  const [count, setCount] = useState(4);
+  const [theme, setTheme] = useState(" blue");
 
   function decrementCount() {
-    setCount((prevCount) => count - 1);
+    setCount((prevCount) => prevCount - 1);
+    setTheme(" red");
   }
 
   function incrementCount() {
-    setCount((prevCount) => count + 1);
+    setCount((prevCount) => prevCount + 1);
   }
 
   return (
     <>
       <button onClick={decrementCount}>-</button>
       <span>{count}</span>
+      <span>{theme}</span>
       <button onClick={incrementCount}>+</button>
     </>
   );
